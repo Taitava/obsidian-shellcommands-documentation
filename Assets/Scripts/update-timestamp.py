483293affe25225a92d1f7e6bbd28245405fd437
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-12-11 13:36:12 +0200

    update-timestamp.py: Rename variables.

--- Assets/Scripts/update-timestamp.py
+++ Assets/Scripts/update-timestamp.py
@@ -4,39 +4,39 @@ import sys
 import subprocess
 from datetime import date
 
-count_needed_arguments = 1
-if len(sys.argv)-1 != count_needed_arguments:
-    sys.exit("This script requires "+str(count_needed_arguments)+" arguments: a file path.")
+countNeededArguments = 1
+if len(sys.argv)-1 != countNeededArguments:
+    sys.exit("This script requires "+str(countNeededArguments)+" arguments: a file path.")
 
-file_path = sys.argv[1]
+filePath = sys.argv[1]
 
 # Get the current date
-current_date = date.today().isoformat()
+currentDate = date.today().isoformat()
 
 # Get the creation date from git (if possible)
-git_dates = str(subprocess.run(["git", "log", "--follow", "--format=%ad", "--date", "format:%Y-%m-%d", "--reverse", file_path], stdout=subprocess.PIPE).stdout).strip('\\n')
-if re.match(r"^b'(\d{4}-\d{2}-\d{2})", git_dates):
+gitDates = str(subprocess.run(["git", "log", "--follow", "--format=%ad", "--date", "format:%Y-%m-%d", "--reverse", filePath], stdout=subprocess.PIPE).stdout).strip('\\n')
+if re.match(r"^b'(\d{4}-\d{2}-\d{2})", gitDates):
     # Git was able to find dates
-    creation_date = re.search(r"^b'(\d{4}-\d{2}-\d{2})", git_dates).group(1)
+    creationDate = re.search(r"^b'(\d{4}-\d{2}-\d{2})", gitDates).group(1)
 else:
     # Git did not find a creation date for the file, so the file is not in git yet.
     # Use the current date as the creation date.
-    creation_date = current_date
+    creationDate = currentDate
 
 # Read the file in memory
-with open(file_path, "r", newline="\n") as read_file:
-    file_content = read_file.read()
+with open(filePath, "r", newline="\n") as readFile:
+    fileContent = readFile.read()
 
 # Change or add a timestamp in the content
-new_timestamp = "<small>This page was last modified on " + current_date + " and created on " + creation_date + ".</small>"
-regex_pattern = r"^<small>This page was last modified on \d{4}-\d{2}-\d{2} and created on \d{4}-\d{2}-\d{2}.</small>$"
-if re.match(regex_pattern, file_content, re.MULTILINE):
+newTimestamp = "<small>This page was last modified on " + currentDate + " and created on " + creationDate + ".</small>"
+regexPattern = r"^<small>This page was last modified on \d{4}-\d{2}-\d{2} and created on \d{4}-\d{2}-\d{2}.</small>$"
+if re.match(regexPattern, fileContent, re.MULTILINE):
     # An old timestamp exists - update it.
-    re.sub(regex_pattern, new_timestamp, file_content)
+    re.sub(regexPattern, newTimestamp, fileContent)
 else:
     # No timestamp exists yet - add one.
-    file_content = new_timestamp + "\n" + file_content
+    fileContent = newTimestamp + "\n" + fileContent
 
 # Write the modified file
-with open(file_path, "w", newline="\n") as write_file:
-    write_file.write(file_content)
+with open(filePath, "w", newline="\n") as writeFile:
+    writeFile.write(fileContent)
