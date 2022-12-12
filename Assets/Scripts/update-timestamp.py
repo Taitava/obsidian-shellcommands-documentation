7959ed241ee4564c8eaa4e87488fe796ff8ca719
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-12-12 18:34:49 +0200

    update-timestamp.py: Take both creation and modification times from git log.
    Before this, the modification time was always the current time.

--- Assets/Scripts/update-timestamp.py
+++ Assets/Scripts/update-timestamp.py
@@ -10,25 +10,23 @@ if len(sys.argv)-1 != countNeededArguments:
 
 filePath = sys.argv[1]
 
-# Get the current date
-currentDate = date.today().isoformat()
-
-# Get the creation date from git (if possible)
-gitDates = str(subprocess.run(["git", "log", "--follow", "--format=%ad", "--date", "format:%Y-%m-%d", "--reverse", filePath], stdout=subprocess.PIPE).stdout).strip('\\n')
-if re.match(r"^b'(\d{4}-\d{2}-\d{2})", gitDates):
+# Get the creation and modification dates from git (if possible)
+gitLogOutput = subprocess.run(["git", "log", "--follow", "--format=%ad", "--date", "format:%Y-%m-%d", filePath], stdout=subprocess.PIPE).stdout.decode("utf-8").strip('\n')
+if re.match(r"^(\d{4}-\d{2}-\d{2})", gitLogOutput):
     # Git was able to find dates
-    creationDate = re.search(r"^b'(\d{4}-\d{2}-\d{2})", gitDates).group(1)
+    gitDates = gitLogOutput.split('\n')
+    modificationDate = gitDates[0]
+    creationDate = gitDates[-1]
 else:
-    # Git did not find a creation date for the file, so the file is not in git yet.
-    # Use the current date as the creation date.
-    creationDate = currentDate
+    # Git did not find dates for the file, so the file is not in git yet.
+    sys.exit("Unable to generate timestamp for file '"+filePath+"' because it doesn't seem to be committed to git.")
 
 # Read the file in memory
 with open(filePath, "r", newline="\n") as readFile:
     fileContent = readFile.read()
 
 # Change or add a timestamp in the content
-newTimestamp = "<small>This page was last modified on " + currentDate + " and created on " + creationDate + ".</small>\n"
+newTimestamp = "<small>This page was last modified on " + modificationDate + " and created on " + creationDate + ".</small>\n"
 regexPattern = r"(?<=^# History)(?P<middleSpacing>\s+)(<small>[^<]*</small>)?\n?"
 regexModifiers = re.MULTILINE | re.DOTALL
 if re.search(regexPattern, fileContent, regexModifiers):
